08c8f283899b90adc3e56cfa499cf1e2dad4ee27
victorrony <victor.fernandes@sintaxy.com>
2024-09-24 15:55:37 -0100

    alterar URL_BASE

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -2,7 +2,6 @@ export const Enviroment = {
    LIMITE_DE_LINHAS: 5, //Define a quantidade de linhas a ser carredo por padrao nas listagens.
    INPUT_DE_BUSCA: "Pesquisar...", //Placeholder exibido nas inputs
    LISTAGEM_VAZIA: "Nenhum registro encontrado.", //text axibido quando nenhum registro é encontrado em uma listagem. 
-   URL_BASE: 'https://listagem-de-pessoas-e-git-a9b230-victorrony89gmailcoms-projects.vercel.app',   
-//Url base da consulta dos dados dessa aplicação.
+   URL_BASE: 'https://listagem-de-pessoas-e-cidades-back-foqsug9b5.vercel.app',   //Url base da consulta dos dados dessa aplicação.
    // URL_BASE: "http://localhost:3335", //Url base da consulta dos dados dessa aplicação.
 };
